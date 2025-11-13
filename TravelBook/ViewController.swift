18dae0c98c7e7cf680bb4fee4436a428a9740e13
zeynepbetul <z.beetulkaya@gmail.com>
2025-11-13 13:28:36 +0300

    add core location

--- TravelBook/ViewController.swift
+++ TravelBook/ViewController.swift
@@ -7,14 +7,20 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class ViewController: UIViewController, MKMapViewDelegate {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         mapView.delegate = self
+        locationManager.delegate = self
+        
+        
     }
 
 
