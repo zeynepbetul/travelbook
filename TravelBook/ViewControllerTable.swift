9117c42fa55a8617e82db42249fc9536f5ddab55
zeynepbetul <z.beetulkaya@gmail.com>
2025-11-13 15:04:27 +0300

    add new view table and segue

---
+++ TravelBook/ViewControllerTable.swift
@@ -0,0 +1,31 @@
+//
+//  ViewControllerTable.swift
+//  TravelBook
+//
+//  Created by Zeynep Betül Kaya on 13.11.2025.
+//
+
+import UIKit
+
+class ViewControllerTable: UIViewController {
+
+    @IBOutlet weak var list: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
