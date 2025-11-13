0ad9071f8d0bde944fd45b6c0101ae81c3c28066
zeynepbetul <z.beetulkaya@gmail.com>
2025-11-13 18:27:07 +0300

    open maps to saved coordinate

--- TravelBook/ViewController.swift
+++ TravelBook/ViewController.swift
@@ -143,6 +143,23 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         }
         return pinView
     }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if selectedTitle != "" {
+            let requestLocation = CLLocation(latitude: annotationLatitude, longitude: annotationLongitude)
+            CLGeocoder().reverseGeocodeLocation(requestLocation) { (placemarks, error) in
+                if let placemark = placemarks {
+                    if placemark.count > 0 {
+                        let newPlacemark = MKPlacemark(placemark: placemark[0])
+                        let item = MKMapItem(placemark: newPlacemark)
+                        item.name = self.annotationTitle
+                        let launchOptions = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]
+                        item.openInMaps(launchOptions: launchOptions)
+                    }
+                }
+            }
+        }
+    }
 
 }
 
