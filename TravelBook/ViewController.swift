605bdcb4b9e7485d02f808be6ccb4813645e4ada
zeynepbetul <z.beetulkaya@gmail.com>
2025-11-13 17:35:45 +0300

    get and show data from db

--- TravelBook/ViewController.swift
+++ TravelBook/ViewController.swift
@@ -22,12 +22,50 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     var selectedTitle = ""
     var selectedTitleID: UUID?
     
+    var annotationTitle = ""
+    var annotationSubTitle = ""
+    var annotationLatitude = Double()
+    var annotationLongitude = Double()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if selectedTitle != "" {
-            let stringUUId = selectedTitleID!.uuidString
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Places")
+            let idString = selectedTitleID!.uuidString
+            fetchRequest.predicate = NSPredicate(format: "id = %@", idString)
+            fetchRequest.returnsObjectsAsFaults = false
             
+            do {
+                let results = try context.fetch(fetchRequest)
+                for result in results as! [NSManagedObject] {
+                    if let title = result.value(forKey: "title") as? String {
+                        annotationTitle = title
+                    }
+                    if let subtitle = result.value(forKey: "subtitle") as? String {
+                        annotationSubTitle = subtitle
+                    }
+                    if let latitude = result.value(forKey: "latitude") as? Double {
+                        annotationLatitude = latitude
+                    }
+                    if let longitude = result.value(forKey: "longitude") as? Double {
+                        annotationLongitude = longitude
+                    }
+                    let annotation = MKPointAnnotation()
+                    annotation.title = annotationTitle
+                    annotation.subtitle = annotationSubTitle
+                    let coordinate = CLLocationCoordinate2D(latitude: annotationLatitude, longitude: annotationLongitude)
+                    annotation.coordinate = coordinate
+                    mapView.addAnnotation(annotation)
+                    nameTextField.text = annotationTitle
+                    commentTextField.text = annotationSubTitle
+                }
+                
+            } catch {
+                
+            }
         }
         
         mapView.delegate = self
